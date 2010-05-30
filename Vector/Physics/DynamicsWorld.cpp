d72f5da90a5dfefbbc7a3484ee9591edc255a49a
sunside <sunside@89e8501b-ea7b-4f45-b1b2-b812960b401a>
2010-05-30 02:45:52 +0000

    Registrieren von Rigid Bodies
    git-svn-id: https://aquitaine.dnsalias.net/repo/svn/shitstorm/trunk@79 89e8501b-ea7b-4f45-b1b2-b812960b401a

--- Vector/Physics/DynamicsWorld.cpp
+++ Vector/Physics/DynamicsWorld.cpp
@@ -17,6 +17,63 @@ namespace physics {
 	//! Destruktor
 	DynamicsWorld::~DynamicsWorld(void)
 	{
+		destruct();
+	}
+		
+	//! Vernichtet die Dynamikwelt
+	void DynamicsWorld::destruct() {
+		if (!dynamicsWorld) return;
+
+		// Dispatcher löschen
+		void* dispatcher = dynamicsWorld->getDispatcher();
+		ASSERT(dispatcher);
+		delete dispatcher;
+
+		// Broadphase löschen
+		void* broadphase = dynamicsWorld->getBroadphase();
+		ASSERT(broadphase);
+		delete broadphase;
+
+		// Solver löschen
+		void* solver = dynamicsWorld->getConstraintSolver();
+		ASSERT(solver);
+		delete solver;
+
+		// Bodies entfernen
+		while (!rigidBodies.empty()) {
+			btRigidBody* body = rigidBodies.back();
+			rigidBodies.pop_back();
+			dynamicsWorld->removeRigidBody(body);
+		}
+
+		// Welt vernichten.
+		delete dynamicsWorld;
+		dynamicsWorld = NULL;
+
+		// Collision Configuration vernichten
+		ASSERT(collisionConfiguration);
+		delete collisionConfiguration;
+		collisionConfiguration = NULL;
 	}
 
+	//! Erzeugt die DefaultCollisionConfiguration
+	btDefaultCollisionConfiguration* DynamicsWorld::createCollisionConfiguration() {
+		return new btDefaultCollisionConfiguration();
+	}
+
+	//! Erzeugt die Welt
+	void DynamicsWorld::createWorld() {
+
+		// Collision Configuration erzeugen
+		collisionConfiguration = createCollisionConfiguration();
+	}
+
+	//! Fügt der Welt einen Rigid Body hinzu
+	void DynamicsWorld::addRigidBody(btRigidBody* body) {
+		ASSERT(dynamicsWorld);
+		ASSERT(body);
+
+		dynamicsWorld->addRigidBody(body);
+		rigidBodies.push_back(body);
+	}
 }}
--- Vector/Physics/DynamicsWorld.h
+++ Vector/Physics/DynamicsWorld.h
@@ -11,6 +11,12 @@
 #define _DYNAMICSWORLD_H
 
 #include "global.h"
+#include <vector>
+
+// Vorwärtsdeklaration der Klassen
+class btDiscreteDynamicsWorld;
+class btDefaultCollisionConfiguration;
+class btRigidBody;
 
 namespace pv {
 namespace physics {
@@ -21,11 +27,41 @@ namespace physics {
 	public:
 
 		//! Erzeugt eine neue Instanz des Objektes
-		DynamicsWorld(void) {}
+		DynamicsWorld(void) : dynamicsWorld(NULL), collisionConfiguration(NULL) {}
 
 		//! Destruktor
 		virtual ~DynamicsWorld(void);
+
+		//! Erzeugt die Welt
+		void createWorld();
+
+		//! Ermittelt, ob die Dynamikwelt initialisiert wurde
+		inline bool isInitialized() const { return dynamicsWorld != NULL; }
+
+		//! Fügt der Welt einen Rigid Body hinzu
+		void addRigidBody(btRigidBody* body);
+
+	protected:
+
+		//! Erzeugt die DefaultCollisionConfiguration
+		virtual btDefaultCollisionConfiguration* createCollisionConfiguration();
+
+	private:
 		
+		//! Vernichtet die Dynamikwelt
+		void destruct();
+
+	private:
+
+		//! Die tatsächlich verwendete Dynamikwelt
+		btDiscreteDynamicsWorld* dynamicsWorld;
+
+		//! Die Collision Configuration
+		btDefaultCollisionConfiguration* collisionConfiguration;
+
+		//! Sammlung aller Rigid Bodies
+		std::vector<btRigidBody*> rigidBodies;
+
 	};
 
 }}
