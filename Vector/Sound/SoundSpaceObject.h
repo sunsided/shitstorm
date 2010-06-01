f753cd0eb57e87c01b087f1110c148757e445acd
sunside <sunside@89e8501b-ea7b-4f45-b1b2-b812960b401a>
2010-06-01 22:53:18 +0000

    Interface der SoundSpaceObject-Klasse
    git-svn-id: https://aquitaine.dnsalias.net/repo/svn/shitstorm/trunk@139 89e8501b-ea7b-4f45-b1b2-b812960b401a

--- Vector/Sound/SoundSpaceObject.h
+++ Vector/Sound/SoundSpaceObject.h
@@ -16,16 +16,34 @@
 namespace pv {
 namespace sound {
 
-//! Soundobjekt im 3D-Raum
-class SoundSpaceObject
-{
-public:
-	//! Erzeugt eine neue Instanz des Objektes
-	SoundSpaceObject(void);
+	//! Soundobjekt im 3D-Raum
+	class SoundSpaceObject
+	{
+	protected:
 
-	// Destruktor
-	virtual ~SoundSpaceObject(void);
-};
+		//! Erzeugt eine neue Instanz des Objektes
+		SoundSpaceObject(void);
+
+	public:
+
+		// Destruktor
+		virtual ~SoundSpaceObject(void);
+
+		//! Setzt die Verstärkung
+		virtual void setGain(irr::f32 gain) = 0;
+
+		//! Setzt die Position
+		inline void setPosition(irr::core::vector3df &origin) { setPosition(origin.X, origin.Y, origin.Z); }
+
+		//! Setzt die Position
+		virtual void setPosition(irr::f32 x, irr::f32 y, irr::f32 z) = 0;
+
+		//! Setzt die Geschwindigkeit
+		inline void setVelocity(irr::core::vector3df &veclocity) { setVelocity(veclocity.X, veclocity.Y, veclocity.Z); }
+
+		//! Setzt die Geschwindigkeit
+		virtual void setVelocity(irr::f32 x, irr::f32 y, irr::f32 z) = 0;
+	};
 
 }}
 
