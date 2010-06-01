2a9445c5f936f9a93173ed67bbe77b8b41e8fe16
sunside <sunside@89e8501b-ea7b-4f45-b1b2-b812960b401a>
2010-06-01 23:07:23 +0000

    SoundListener modelliert
    git-svn-id: https://aquitaine.dnsalias.net/repo/svn/shitstorm/trunk@140 89e8501b-ea7b-4f45-b1b2-b812960b401a

--- Vector/Sound/SoundListener.cpp
+++ Vector/Sound/SoundListener.cpp
@@ -6,20 +6,55 @@
  * $Id$
  */
 
-
 #include "SoundListener.h"
+
+#include <al.h>
 
 namespace pv {
 namespace sound {
 
-SoundListener::SoundListener(void)
-{
-}
+	SoundListener::SoundListener(void)
+	{
+	}
 
 
-SoundListener::~SoundListener(void)
-{
-}
+	SoundListener::~SoundListener(void)
+	{
+	}
 
+	//! Setzt die Verstärkung
+	void SoundListener::setGain(irr::f32 gain) {
+	}
+
+	//! Setzt die Position
+	void SoundListener::setPosition(irr::f32 x, irr::f32 y, irr::f32 z) {
+		alListener3f(AL_POSITION, x, y, z);
+	}
+
+	//! Setzt die Geschwindigkeit
+	void SoundListener::setVelocity(irr::f32 x, irr::f32 y, irr::f32 z) {
+		alListener3f(AL_VELOCITY, x, y, z);
+	}
+
+	//! Setzt die Richtung zurück
+	void SoundListener::resetOrientation() {
+		
+		irr::f32 vec[6] = {0}; // Initialisiert alle Felder auf 0
+		alListenerfv(AL_ORIENTATION, vec);
+	}
+
+	//! Setzt die Richtung
+	void SoundListener::setOrientation(irr::core::vector3df& forward) {
+		
+		irr::f32 vec[6] = { forward.X, forward.Y, forward.Z, 0, 1, 0 };
+		alListenerfv(AL_ORIENTATION, vec);
+	}
+
+	//! Setzt die Richtung
+	void SoundListener::setOrientation(irr::core::vector3df& forward, irr::core::vector3df& up) {
+		
+		irr::f32 vec[6] = { forward.X, forward.Y, forward.Z, up.X, up.Y, up.Z };
+		alListenerfv(AL_ORIENTATION, vec);
+	}
 
 }}
--- Vector/Sound/SoundListener.h
+++ Vector/Sound/SoundListener.h
@@ -17,16 +17,40 @@
 namespace pv {
 namespace sound {
 
-//! Sound Listener
-class SoundListener : public SoundSpaceObject
-{
-public:
-	//! Erzeugt eine neue Instanz des Objektes
-	SoundListener(void);
+	//! Sound Listener
+	/**
+	 * Diese Klasse steuert den Listener des aktuell aktivierten Kontextes.
+	 * Ist die Steuerung eines spezifischen Kontextes nötig, kann eine
+	 * Instanz des @see ContextSwitchingSoundListener verwendet werden.
+	 */
+	class SoundListener : public SoundSpaceObject
+	{
+	public:
+		//! Erzeugt eine neue Instanz des Objektes
+		SoundListener(void);
 
-	// Destruktor
-	virtual ~SoundListener(void);
-};
+		// Destruktor
+		virtual ~SoundListener(void);
+
+
+		//! Setzt die Verstärkung
+		virtual void setGain(irr::f32 gain);
+
+		//! Setzt die Position
+		virtual void setPosition(irr::f32 x, irr::f32 y, irr::f32 z);
+
+		//! Setzt die Geschwindigkeit
+		virtual void setVelocity(irr::f32 x, irr::f32 y, irr::f32 z);
+
+		//! Setzt die Richtung zurück
+		virtual void resetOrientation();
+
+		//! Setzt die Richtung
+		virtual void setOrientation(irr::core::vector3df& forward);
+
+		//! Setzt die Richtung
+		virtual void setOrientation(irr::core::vector3df& forward, irr::core::vector3df& up);
+	};
 
 }}
 
