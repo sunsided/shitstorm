d788bb154ed54a70fe6b285099e852d94d9d32c4
sunside <sunside@89e8501b-ea7b-4f45-b1b2-b812960b401a>
2010-06-07 02:31:38 +0000

    LinkerFehlerSuche1 gebrancht
    git-svn-id: https://aquitaine.dnsalias.net/repo/svn/shitstorm/branches/LinkerFehlerSuche1@193 89e8501b-ea7b-4f45-b1b2-b812960b401a

---
+++ Vector/Scripting/ScriptingVM.h
@@ -0,0 +1,48 @@
+/** 
+ * Project Vector
+ * Scripting-VM
+ *
+ * (c) 2010, Markus Mayer <code@defx.de>
+ * $Id$
+ */
+
+#pragma once
+#ifndef _SCRIPTINGVM_H
+#define _SCRIPTINGVM_H
+
+#include "global.h"
+
+// SQUIRREL!
+#include <squirrel.h> 
+
+namespace pv {
+namespace scripting {
+
+	//! Virtuelle Maschine für das Scripting
+	class ScriptingVM
+	{
+	public:
+		//! Erzeugt eine neue Instanz der VM
+		ScriptingVM();
+
+		//! Destruktor
+		virtual ~ScriptingVM();
+		
+		//! Initialisiert die Engine.
+		/** Muss als erste Funktion aufgerufen werden.
+		 * @param stackSize		Die initiale Größe des Stacks
+		 */
+		void initialize(irr::u32 stackSize = 1024);
+
+		//! Deinitialisiert die VM
+		void terminate();
+
+	private:
+
+		//! Die Squirrel-VM
+		HSQUIRRELVM *vm;
+	};
+
+}}
+
+#endif
--- Vector/main.cpp
+++ Vector/main.cpp
@@ -4,21 +4,31 @@
  * $Id$
  */
 
-#include "global.h"
+
+//#include "global.h"
 #include "linker.h"
 
-#include "GameLogic/GameEngine.h"
-#include "Sound/SoundDeviceManager.h"
-#include "Sound/OggVorbisAudioSource.h"
+//#include "GameLogic/GameEngine.h"
+//#include "Scripting/ScriptingVM.h"
+#include <sqplus.h>
 
+/*
 #include <memory>
 #include <iostream>
 
 using namespace std;
-using namespace pv;
+*/
+//using namespace pv;
 
 int main(int argc, char **argv) {
 
+	/*
+	scripting::ScriptingVM *vm = new scripting::ScriptingVM();
+	vm->initialize();
+	vm->terminate();
+	delete vm;
+	*/
+	/*
 	// Engine erzeugen
 	auto_ptr<EngineBase> engine( new GameEngine() );
 	if (!engine.get()) {
@@ -41,4 +51,6 @@ int main(int argc, char **argv) {
 
 	// Engine freigeben und Spiel beenden
 	return result;
+	*/
+	return 1;
 }
